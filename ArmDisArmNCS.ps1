a08e481054ca8e2fdc55d28cd57a9bb6b45757a5
Seinfeld 神馬Marc <32960421+PapaMarc@users.noreply.github.com>
2020-08-29 16:46:10 +0800

    initial code upload and readme tweaks

---
+++ ArmDisArmNCS.ps1
@@ -0,0 +1,207 @@
+
+<#
+script ARMing and DisARMing of the NetCamStudio security system
+lots to learn. work in progress.
+With any luck, this will give you a head start at a common end-user scenario. Enjoy. Marc
+#>
+
+#----------------------------------------------
+#Import the GUI Assemblies
+#----------------------------------------------
+
+    
+ #   Add-Type -AssemblyName PresentationCore,PresentationFramework,WindowsBase | Out-Null
+    Add-Type -AssemblyName System.Drawing | Out-Null
+    Add-Type -AssemblyName System.Windows.Forms | Out-Null
+
+#[void][reflection.assembly]::Load('System.Windows.Forms, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089')
+#[System.WIndows.Forms.Application]::EnableVisualStyles()
+
+#
+#Define Variables, Arrays, URLs etc
+#Define vars
+#Definie NetCamStudio specific logon related vars
+$MyNCSun = 'ReplaceWithYourNCSUserName'
+$MyNCSpwd = 'ReplaceWithYourNCSPassword'
+$MyNCSAuthToken = 'ReplaceWithYourNCSAuthToken'
+
+#Define application specific vars
+$ExitDelayInSec = 8
+$Action = 'Command Me'
+
+# Define Actions Array
+#$Array_Actions = @('NCS Service Status','NCS Service Start','NCS Service Stop','NCS Logon','NCS Enumerate Cams','ARM NCS','ARM NCS with DELAY','DisARM NCS')
+
+#Define URLS
+#Logon URL
+$URLLogon = "http://localhost:8124/Json/Login?username="$MyNCSun"%38password="$MyNCSpwd""
+
+#Combo Cams URL
+$URLGetCams = "http://localhost:8124/Json/GetCameras?authToken="$MyNCSAuthToken""
+
+#Set CamN for 'enable motion detection' (which facilitates recording to occur on motion)
+$URLCam0 = "http://localhost:8124/Json/StartStopMotionDetector?sourceId=0&enabled=true&authToken="$MyNCSAuthToken""
+$URLCam1 = "http://localhost:8124/Json/StartStopMotionDetector?sourceId=1&enabled=true&authToken="$MyNCSAuthToken""
+#$URLCam2 = "http://localhost:8124/Json/StartStopMotionDetector?sourceId=2&enabled=true&authToken="$MyNCSAuthToken""
+#$URLCam3 = "http://localhost:8124/Json/StartStopMotionDetector?sourceId=3&enabled=true&authToken="$MyNCSAuthToken""
+
+#disables motion detector on cameras causing motion triggered recording rules to no longer fire
+$URLCam0 = "http://localhost:8124/Json/StartStopMotionDetector?sourceId=0&enabled=false&authToken="$MyNCSAuthToken""
+$URLCam1 = "http://localhost:8124/Json/StartStopMotionDetector?sourceId=1&enabled=false&authToken="$MyNCSAuthToken""
+#$URLCam2 = "http://localhost:8124/Json/StartStopMotionDetector?sourceId=2&enabled=false&authToken="$MyNCSAuthToken""
+#$URLCam3 = "http://localhost:8124/Json/StartStopMotionDetector?sourceId=3&enabled=false&authToken="$MyNCSAuthToken""
+
+
+# Function Definitions
+
+#
+# Set ComboBox selection to var and take action
+function Return_Combo () {
+    $Action = $ComboBox.Text
+    $Label_ArmStatus.Text = $Action
+    $Label_ArmStatus.Refresh()
+     If ( 0 -eq $ComboBox.SelectedIndex )
+     {
+       ServiceStatus
+     }
+      Elseif ( 1 -eq $ComboBox.SelectedIndex )
+      {
+        ServiceStart
+      }
+     Elseif ( 6 -eq $ComboBox.SelectedIndex )
+      {
+        ExitDelayCountdown
+      }
+  }
+#
+# Service Status (ComboBox Index=0)
+function ServiceStatus () {
+   Get-Service NetcamStudioSvc | Write-Output
+}
+
+#
+# Start Status (ComboBox Index=1)
+function ServiceStart () {
+  Start-Service NetcamStudioSvc
+}
+
+#
+# Stop Status (ComboBox Index=2)
+function ServiceStop () {
+  Stop-Service "NetcamStudioSvc"
+}
+#
+#Logon NetCam Studio (NCS) (ComboBox Index=3)
+Function Logon () {
+  PowerShell Invoke-WebRequest -Uri $URLLogon -Method GET
+}
+#
+#Get Cams  (ComboBox Index=4)
+Function ComboCams () {
+  PowerShell Invoke-WebRequest -Uri $URLComboCams -Method GET
+}
+
+#
+
+# Exit Delay Countdown Function (AND ARM) (ComboBox Index=6)
+#Clear-Host
+Function ExitDelayCountdown () {
+  1..$ExitDelayInSec | ForEach-Object { 
+    Start-Sleep -s 1
+    $TMinus=$ExitDelayInSec-$_
+    Write-Progress -activity "Arming System in: " -Status ($TMinus)
+#   Write-Progress -Activity  -SecondsRemaining ($ExitDelayInSec) -PercentComplete ($ExitDelayInSec)
+#   update the UI
+    $Label_ArmStatus.Text = "Arming System in: $TMinus"
+       $Label_ArmStatus.Refresh()
+#   Added exit delay... NOW do the real work / call the ARM function...
+#   ArmNCS ()
+  }
+}
+
+#
+# Arm NCS  (ComboBox Index=5)
+# Arms NCS on departure from apartment
+
+#Enable source: PC/tablet built-in screen webcam - enable/disable doesn't work
+#$URLEnableCam3 = "http://localhost:8124/Json/ConnectCameraJson?sourceId=3&enabled=true&authToken="$MyNCSAuthToken""
+#Invoke-WebRequest -Uri $URLEnableCam3
+
+Function ArmNCS () {
+Write-Host 'Arming System...'
+#non-verbose
+  Invoke-WebRequest -Uri $URLCam0
+  Invoke-WebRequest -Uri $URLCam1
+  #Invoke-WebRequest -Uri $URLCam2
+  #Invoke-WebRequest -Urk $UrlCam3
+Write-Host 'System ARMED!'
+    $Label_ArmStatus.Text = 'System ARMED!'
+    $Label_ArmStatus.Refresh()
+}
+
+#
+# DisArm NCS  (ComboBox Index=7)
+Function DisArmNCS () {
+  Invoke-WebRequest -Uri $URLCam0
+  Invoke-WebRequest -Uri $URLCam1
+  #Invoke-WebRequest -Uri $URLCam2
+  Write-Host 'System disarmed'
+    $Label_ArmStatus.Text = 'System disarmed'
+    $Label_ArmStatus.Refresh()
+}
+
+
+#___________
+#
+# Main
+#
+#___________
+
+# Define and Draw Form
+$Form_ArmDisArm = New-Object System.Windows.Forms.Form
+  $Form_ArmDisArm.Text = "Arm / DisArm Console - NetCam Studio"
+  $Form_ArmDisArm.Size = New-Object System.Drawing.Size(500,300)
+  
+$ComboBox = New-Object System.Windows.Forms.ComboBox
+  $ComboBox.Location = New-Object System.Drawing.Point(10,40)
+  $ComboBox.Size = New-Object System.Drawing.Size(280,20)
+  $ComboBox.Height = 80
+#  [void] $ComboBox.Items.AddRange($Array_Actions[0..7])
+  [void] $ComboBox.Items.Add('NCS Service Status')
+  [void] $ComboBox.Items.Add('NCS Service Start')
+  [void] $ComboBox.Items.Add('NVS Service Stop')
+  [void] $ComboBox.Items.Add('NCS Logon')
+  [void] $ComboBox.Items.Add('NCS Enumerate Cams')
+  [void] $ComboBox.Items.Add('ARM NCS')
+  [void] $ComboBox.Items.Add('ARM NCS with DELAY')
+  [void] $ComboBox.Items.Add('DisARM NCS')
+
+$Label_ArmStatus = New-Object System.Windows.Forms.Label
+  $Label_ArmStatus.Text = 'Awaiting your command'
+  $Label_ArmStatus.Location = New-Object System.Drawing.Size(100,200)
+  $Label_ArmStatus.Size = New-Object System.Drawing.Size(300,30)
+#  $Label_ArmStatus.AutoSize = $true
+  $Label_ArmStatus.Font = [System.Drawing.Font]::new("Microsoft Sans Serif", 16, [System.Drawing.FontStyle]::Bold)
+
+$Button = new-object System.Windows.Forms.Button
+  $Button.Size = new-object System.Drawing.Size(100,40)
+  $Button.Location = new-object System.Drawing.Size(130,100)
+  $Button.Text = "Execute"
+ 
+
+
+  $Form_ArmDisArm.Controls.Add($Label_ArmStatus)
+  $Form_ArmDisArm.Controls.Add($ComboBox)
+  $Form_ArmDisArm.Controls.Add($Button)
+  $Button.Add_Click({Return_Combo})
+  $Form_ArmDisArm.ShowDialog()
+
+
+
+
+
+
+
+
+
+       
--- README.md
+++ README.md
@@ -15,35 +15,25 @@ $MyNCSun = 'ReplaceWithYourNCSUserName'
 $MyNCSpwd = 'ReplaceWithYourNCSPassword'
 $MyNCSAuthToken = 'ReplaceWithYourNCSAuthToken'
 used to access the target NetCam Studio service with the account you established above/beyond the default NCS Admin acct.
-Set these, along with <$DelayInSec>, to your personal/preferred values.
+Set these, along with $ExitDelayInSec, to your personal/preferred values.
 
 
-Installation:
-
+Installation... a kin to my favorite sushi recipe... 'catch fish. serve.'  :-)
     Place in the folder of your choice:
         ArmDisArmNCS.ps1             PowerShell script
-#       ArmDisArmNCS                 Shortcut
-
-        
-    Modify the Shortcut to point to this folder.
-    
-    Copy the Shortcut to the desktop or where ever you like
-
-
-
-To execute:
-
-    Run the PowerShell script which should happen by clicking the shortcut.
-
+    Run script.
 
 
 
 FUTURE todos moving fwd... in no particular order:
     
-    -Cleanup formatting.
-    -pursue some degree elegance, as at present there's none ;-( eg. Get Array working to populate ComboBox v. manually filling it, and similar throughout... this is a learning PowerShell, VSCode, GitHub from VSCode exercise for me.
-    -Make User ID and password handling more secure; require pwd to DisARM (currently rely on WinDesktop lockout/pwd to get back to console running on WinDesktop so anyone with access can hit the button and disarm)
-    -work through issues, which i presently suspect are on the NetCamStudio side, wrt enabling/disabling a camera all together. eg. i don't want my laptop webcam always connected to the NCS server. I ONLY want to enable it, and then enable motion detection on it-- WHEN i want to ARM the system and depart. I DO NOT want that cam connected to the NetcamStudioSvc (and watching me at the computer) at ANY other time, nor do i want that service to 'hold onto' that camera which i may want to use with/for other apps like a Zoom call, etc. But when i leave my house, let alone my desk-- i want to know if/when anyone sits in front of the computer, so at that point on ARM'ing the system, i'd like to attach the camera to NCS, and include it as part of the ARM'ed system. More on that here:
+    0) pursue some degree elegance, as at present there is precisely zero ;-( Though i'm learning ins/outs of the PowerShell, VSCode, the debugger, GitHub, etc. This is a long overdue exercise for me learning PowerShell, VSCode, GitHub from VSCode, etc.
+    1) Get Array working to populate ComboBox v. manually filling it, and similar learning curve throughout... 
+    2) No current error handling whatsoever... i know. doh... at some point as part of the exercise will review https://www.varonis.com/blog/powershell-try-catch
+    3) Cleanup formatting and related conventions... it's a mess.
+    4) Make User ID and password handling more secure; require pwd to DisARM (currently relying on WinDesktop lockout/pwd to get back to console running on WinDesktop so anyone with access can hit the button and disarm. And look through code and see creds, etc.)
+    5) work through issues, which i presently suspect are on the NetCamStudio side, wrt enabling/disabling a camera all together.
+    eg. i don't want my laptop webcam always connected to the NCS server. I ONLY want to enable it, and then enable motion detection on it-- WHEN i want to ARM the system and depart. I DO NOT want that cam connected to the NetcamStudioSvc (and watching me at the computer) at ANY other time, nor do i want that service to 'hold onto' that camera which i may want to use with/for other apps like a Zoom call, etc. But when i leave my house, let alone my desk-- i want to know if/when anyone sits in front of the computer, so at that point on ARM'ing the system, i'd like to attach the camera to NCS, and include it as part of the ARM'ed system. More on that here:
     https://community.netcamstudio.com/t/cant-enable-disable-a-configured-cam-video-source-via-webapi/3895
 
 
